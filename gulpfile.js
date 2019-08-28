264da2e6f73016cb3d769636a40fb467185fc436
digicontributer <esot@digibyte.io>
2019-08-28 10:48:59 +1000

    fix callback in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var errors = require('./index.js')
 var path = require('path')
 
 gulp.task('generate-docs', function (cb) {
-  errors.generateDocs(errors, path.join(__dirname, 'docs', 'errors.md'))
+  errors.generateDocs(errors, path.join(__dirname, 'docs', 'errors.md'), function(err) {
+
+  });
 })
 
 gulp.task('prepublish', ['generate-docs'])
